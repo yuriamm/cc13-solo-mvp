5fcd5cf6c673aed4abe6ce32a636d5826ed3c5e3
Yuri Amami <yuriamami@Yuris-MacBook-Pro.local>
2020-08-11 11:00:02 -0400

    another try to solve Heroku error

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
+// require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
 const app = require("./app");
 const db = require("./db");
 
